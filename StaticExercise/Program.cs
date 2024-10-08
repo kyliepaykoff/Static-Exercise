846e18a61ee111c389bd0dfdc43decc016a8f4e0
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-10-07 21:48:37 -0400

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Console.WriteLine($"{TempConverter.FahrenheitToCelcius(72)}");
+            Console.WriteLine($"72 degrees fahrenheit to celcius");
+
+
+            Console.WriteLine($"{TempConverter.CelciusToFahrenheit(22)}");
+            Console.WriteLine($"22 degrees celcius to fahrenheit");
+
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+
+
+
+        }
+
+
+
+
+        public static double CelciusToFahrenheit(double celcius)
+        {
+
+            return (celcius * 9) / 5 + 32;
+
+        }
+
+
+
+
+
+    }
+}
